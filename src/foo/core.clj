d710f3b1781c3174f73820640dee9f690e93e9cf
Kirk Dietz <kdietz1@email.davenport.edu>
2015-07-27 07:59:06 -0400

    Back-tracking bug removed. All tests passed. Console output is still buggy

--- src/foo/core.clj
+++ src/foo/core.clj
@@ -1,200 +1,131 @@
 (ns foo.core)
 
-(defn join-values "joins two values as a vector" [a b]
-  [a b])
+(defn mask "bit-masking." [masks bools idx]
+  (if-not (= (count masks) (count bools)) (throw (ArgumentException. "'masks' and 'bools' must match in size")))
+  (if-not (integer? idx) (throw (ArgumentException. "idx must be an integer")))
 
-(defn inc-non-zero "increases a by b, only if a is not zero" [a b]
-  (if-not (= a 0)
-    (+ a b)
-    (* 0 0)))
+  (for [i (range (count bools))]
+    (if (get bools i)
+      (bit-set (get masks i) idx)
+      (get masks i))))
 
-(defn inc-non-zeroes "increases non-zero values of collection using scalar value. returns vector" 
+(defn inc-non-zeroes "increases non-zero values of collection using scalar value" 
   [coll scalar]
-  (map inc-non-zero coll (repeat (count coll) scalar)))
+  (map (fn [a b] (if (not= a 0) (+ a b) 0)) coll (repeat (count coll) scalar)))
 
 (defn gen-item "generates sequence for given weight value and capacity" [weight value capacity]
   (lazy-cat (take weight (repeat 0)) (repeat (- (inc capacity) weight) value)))
 
-(defn gen-items [weights values capacity]
-  (let [n (count weights)]
-    (for [i (range 0 n)]
-      (gen-item (get weights i) (get values i) capacity))))
-
-(defn shift "shifts values in a collection" [previous shift]
+(defn shift "shifts values in a collection" [previous offset]
   (let [capacity (count previous)]
-    (take capacity (lazy-cat (repeat shift 0) previous))))
+    (take capacity (lazy-cat (repeat offset 0) previous))))
 
 (defn get-next [coll weight value]
   (map max
     coll
-    (let [c (dec (count coll))]
-      (gen-item weight value c))    
+    (gen-item weight value (dec (count coll)))
     (inc-non-zeroes (shift coll weight) value)))
 
-(defn deduce [coll args]
+; 1st element: Vector of values (used to calculate changes)
+; 2nd element: Integer of index (used for bitmasking)
+; 3rd element: Vector of bitmask (integers)
+(defn track [coll nextValue]
+  [nextValue 
+  (inc (get coll 1)) 
+  (vec (mask (vec (get coll 2)) (vec (map not= (get coll 0) nextValue)) (get coll 1)))])
+
+(defn deduce "deduces optimal values of next column" [coll args]
   (if-not (vector? args) (throw (ArgumentException. "args should be a vector")))
   (vec (get-next coll (get args 0) (get args 1))))
 
-(defn build-args [weights values capacity]  
-  (vec (conj 
-    (map join-values weights values)
-    (gen-item 0 0 capacity))))
+(defn deduce-and-track "deduces optimal values of next column and tracks change (via bitmask)" [coll args]
+  (if-not (vector? args) (throw (ArgumentException. "args should be a vector")))
+  (let [nextValue (vec (get-next (get coll 0) (get args 0) (get args 1)))]
+    (track coll nextValue)))
 
-(defn get-optimal-value [weights values capacity]
+; data array contains (in order) bitmasks and array of indicies to use for optimal solution
+(defn back-track [data idx]
+  (let [masks (get data 0) weights (get data 2) weight (last (get data 2))]
+    (let [nextWeights (subvec weights 0 (dec (count weights)))]
+      (if (bit-test (last masks) idx)
+          [(subvec masks 0 (- (count masks) weight)) (conj (get data 1) idx) nextWeights]
+          [masks, (conj (get data 1) nil), nextWeights]
+          ))))
+
+(defn run-back-track "drives back-track" [masks weights]
+  (let [n (count weights)]
+    (filter identity
+      (second
+        (reduce back-track (vec (lazy-cat [[masks [] weights]] (take n (iterate dec (- n 1))))))))))
+
+(defn build-args "builds arguments for get-optimal-value" [weights values capacity]  
+  (vec (conj 
+    (map (fn [a b] [a b]) weights values)
+    (vec (gen-item 0 0 capacity)))))
+
+(defn build-tracking-args "builds arguments for get-optimal-solution" [weights values capacity]
+  (vec (conj
+    (map (fn [a b] [a b]) weights values)
+    [(vec (gen-item 0 0 capacity)) 0 (vec (gen-item 0 0 capacity))])))
+
+(defn get-optimal-value "finds the optimal value, but not the optimal solution" [weights values capacity]
   (let [args (build-args weights values capacity)]
     (last (reduce deduce args))))
 
-;(defn fileExists [p] 
-;  (. System.IO.File Exists p))
-
-;(defn parse-int [n] 
-;  (if-not (string? n) (throw (ArgumentException. "Argument must be a string")))
-;  (. Int64 Parse n))
-
-;(defn to-int-array [s]
-;  (if-not (string? s) (throw (ArgumentException. "Argument must be a string")))
-;  (long-array (.Split s (char-array " ") StringSplitOptions/RemoveEmptyEntries)))
-  
-;(defn pathCombine "Create an absolute path (limited to CurrentDirectory)" [p] 
-;  (. System.IO.Path Combine (. Environment CurrentDirectory) "src" "foo" p))
-
-;(defn indexOfMin "returns the index of the lowest value" [v]
-;	(first (apply min-key second (map-indexed vector v))))
-
-;(defn valueOfPreviousColumn "Returns the value of the element in the previous column" [arr x y] 
-;    (if-not arr (throw (NullReferenceException. "Array cannot be null")))
-;    (if-not (integer? x) (throw (ArgumentException. "x must be an integer")))
-;    (if-not (integer? y) (throw (ArgumentException. "y must be an integer")))
-
-;    (if(> x 0) (aget - x 1) 0))
+(defn get-optimal-solution "finds the optimal solutio. returns the optimal value and the indices of items" [weights values capacity]
+  (let [args (build-tracking-args weights values capacity)]
+    (let [result (reduce deduce-and-track args)]
+      (let [bitmask (last result) n (count weights)]
+        [
+        (last (first result))
+        (if (and (> capacity 0) (> n 0)) bitmask )
+        (if (and (> capacity 0) (> n 0))
+          (run-back-track (last result) weights))
+        ]))))
 
 
+(defn write-line [s] (. Console WriteLine s))
+(defn write [s] (. Console Write s))
 
-;(defn back-track [arr]
+(defn parse-int [n] 
+  (if-not (string? n) (throw (ArgumentException. "Argument must be a string")))
+  (. Int64 Parse n))
 
-;  );
+(defn to-int-array [s]
+  (if-not (string? s) (throw (ArgumentException. "Argument must be a string")))
+  (long-array (.Split s (char-array " ") StringSplitOptions/RemoveEmptyEntries)))
 
-;(defn optimal-array-recursive [w v c n]
+(defn print-results [data weights values & names]
+  (write "The optimal value is: ")
+  (write-line (first data))
+  (write "Items to select: ")
+  (write-line (count (last data)))
 
-;  )
+  ;(let [results (reverse (last data))]
+    ;(doseq [i results]
+      ;(write-line i))))
+)
 
-;(defn optimal-array [w v c n]
-;  (println "n: " n "c: " c)
-;  (let [rows (int-array (+ 2 c)) cols (int-array (+ 2 n))]
-;    (let [arr (to-array-2d [cols rows])]
-;      (println (alength arr))
+(defn run-optimization [file]
+  (write "Using the values from file ")
+  (write-line file)
 
-;      (doseq [i (range 0 (+ 1 n))]
-;        (doseq [j (range 0 (+ 1 c))]
-;          (let [h (- i 1)]
-;            (println "i: " i "j: " j)
-;            (cond
-;              (or (= i 0) (= j 0))      ; first row/column are always zero
-                ; these are jagged arrays
-;                (aset arr i j 0)        ; arr[i][j] = 0
-;              (<= (aget w h) j)         ; true: weight can fit
-                ; arr[i][j] = max(v[h] + arr[h][j-w[h]],  arr[h][j]);
- ;               (aset arr i j
-  ;                max(
-   ;                 (+
-    ;                  (aget v h)                      ; v[h]
-     ;                 (aget arr h (- j (aget w h))))  ; arr[h][j-w[h]]
-      ;              (aget arr h j)
-       ;           ))
-        ;    :else
-         ;     (aset arr i j (aget arr h j)) ; arr[i][j] = arr[i-1][j]
-          ;  )
-          ;)
-        ;)
-;      )
-;  arr)))
+  ;Read all lines
+  (let [lines (. System.IO.File ReadAllLines file)]
+    ; files must contain 3 lines
+    (if (< (count lines) 3) (throw (Exception. "File does not have enough data")))
 
-;(defn optimize [w v c n] 
-;  (let [arr (long-array c 0)]
-;    (doseq [i (range 0 n)]
-;      (println (aget w i) "|" (aget v i))
+    (let [capacity (aget lines 0) weights (aget lines 1) values (aget lines 2)]
+      (print-results 
+        (get-optimal-solution (vec weights) (vec values) (parse-int capacity))
+        weights
+        values))))
 
-      ; i > 0
-      ; w[i] + w[i-1] <= c
-
-;      (let [weight (aget w i) canAdd (and (> i 0) (+ weight (aget w (- i 1))))]
-;        (doseq [j (range 0 c)]
-;          (if (<= weight j)
-;            (let 
-;              [
-;                prev (aget arr j) 
-;                maxValue (+ (aget arr (- j weight)) (aget v i))
-;              ]
-;              (if (> maxValue prev)
-;                (aset arr j maxValue)
-;                (println "Row:" j " " prev " --> " maxValue)
-              ;  (
-              ;    (aset arr row maxValue)
-              ;    (aset keep row col true)
-;            ))))))
-              
-
-    ;(spit "log.txt" (. String Join "," arr))
-;    (println (apply max arr))
-;    [arr nil]
-;    ))
-    
-        ; only consider changes if item can fit
-        ;(if (<= weight row)
-         ; (let [maxValue (+ (aget arr (- row weight)) (aget v col))]
-          ;  (if (> maxValue prev)
-           ;   (
-            ;    (aset arr row maxValue)
-             ;   (aset keep row col true)
-              ;  (println "row:" row " col:" col " " maxValue)
-              ;)
-            ;)))))))
-
-;(defn knapsack "0/1 knapsack optimization" [w v c n]
-
-  ; arguments should be of a specific type
-;  (if-not (array? w) (throw (ArgumentException. "Weights 'w' should be an array")))
-;  (if-not (array? v) (throw (ArgumentException. "Values 'v' should be an array")))
-;  (if-not (integer? n) (throw (ArgumentException. "Number of Units 'n' should be an integer")))
-;  (if-not (integer? c) (throw (ArgumentException. "Capacity 'c' should be an integer")))
-
-  ; obey the laws of physics
-;  (if (< c 0) (throw (ArgumentOutOfRangeException. "Capacity must be non-negative")))
-;  (if (< n 0) (throw (ArgumentOutOfRangeException. "Count must be non-negative")))
-
-  ; verification
-;  (if-not (= (count w) n) (throw (ArgumentException. "Weights is wrong size")))
-;  (if-not (= (count v) n) (throw (ArgumentException. "Values is wrong size")))
-;  (if-not (= (count w) (count v)) (throw (Exception. "Weights and Values must have the same amount of elements")))  
-
-;   (if (and (> c 0) (> n 0)) 
-
-;     (optimal-array w v c n) 
-
-
-;     0))
-
-; (defn knapsackFile "0/1 knapsack optimization using input file" [filepath]  
-  
-;   ; path must be a string
-;   (if-not (string? filepath) (throw (ArgumentException. "path must be a string")))
-
-;   (let [p (pathCombine filepath)]
-
-;     ; path must be to an existing file
-;     (if-not (fileExists p) (throw (System.IO.FileNotFoundException. p)))  
-
-;     ; Read all lines
-;     (let [lines (. System.IO.File ReadAllLines p)] 
-
-;       ; files must contain 3 lines
-;       (if-not (= (count lines) 3) (throw (Exception. "Malformed Lines")))
-
-;       (let [capacity (parse-int (aget lines 0)) weights (to-int-array (aget lines 1)) values (to-int-array (aget lines 2))]        
-;         (knapsack weights values capacity (count weights))))))
-
-; (defn -main
-;   [& args]
-;   (if (string? args)
-;     (apply println "Received args:" args))
-;     (println "Argument must be a string to a single file"))
+(defn -main [& args]
+  (write-line "Hello")
+  (if args
+    (if (string? (first args)) 
+      (run-optimization (first args))
+      (write-line "unrecognized argument"))
+    (write-line "no arguments provided"))
+  (write-line "Session Ended"))
--- test/foo/core_test.clj
+++ test/foo/core_test.clj
@@ -2,11 +2,9 @@
   (:use clojure.test
     foo.core))
 
-(deftest increase-non-zero
-  (testing "increase-non-zero test"
-    (is (function? inc-non-zero))
-    (is (= 3 (inc-non-zero 1 2)))
-    (is (= 0 (inc-non-zero 0 3)))))
+(deftest can-read-file-test
+  (let [lines (. System.IO.File ReadAllLines "src//foo//test.txt")]
+    (is (= 3 (count lines)))))
 
 (deftest increase-non-zeroes
   (testing "increase-non-zeroes test"
@@ -22,11 +20,6 @@
     (let [result (vec (gen-item 4 3 6))]
       (is (= (count result) 7))
       (is (= [0 0 0 0 3 3 3])))))
-
-(deftest generate-items-test
-  (testing "gen-items-test"
-    (let [result (gen-items [4 3 2 3] [3 2 4 4] 6)]
-      (is (= (count result) 4)))))      
 
 (deftest shift-test
   (testing "shift-test"
@@ -61,10 +54,31 @@
     (let [result (vec (deduce [0 0 0 2 3 3 3] [2 4]))]
       (is (= result [0 0 4 4 4 6 7])))
     (let [result (vec (deduce [0 0 4 4 4 6 7] [3 4]))]
-      (is (= result [0 0 4 4 4 8 8])))))
+      (is (= result [0 0 4 4 4 8 8])))
+    ))
+
+(deftest tracking-test
+  (testing "tracking-test"
+    (let [result (vec (track [[0 0 0 0 0 0] 0 [0 0 0 0 0 0]] [0 0 0 5 5 5]))]
+      (is (= result [[0 0 0 5 5 5] 1 [0 0 0 1 1 1]])))
+    (let [result (vec (track [[0 0 0 5 5 5] 0 [0 0 0 0 0 0]] [0 0 3 5 5 8]))]
+      (is (= result [[0 0 3 5 5 8] 1 [0 0 1 0 0 1]])))
+    ))
+
+(deftest deduce-and-track-test
+  (testing "deduce-and-track-test"
+    (let [result (vec (deduce-and-track [[0 0 0 0 0 0] 0 [0 0 0 0 0 0]] [3 5]))]
+      (is (= result [[0 0 0 5 5 5] 1 [0 0 0 1 1 1]])))
+    (let [result (vec (deduce-and-track [[0 0 0 5 5 5] 0 [0 0 0 0 0 0]] [2 3]))]
+      (is (= result [[0 0 3 5 5 8] 1 [0 0 1 0 0 1 ]])))
+    (let [result (vec (deduce-and-track [[0 0 3 5 5 8] 0 [0 0 0 0 0 0]] [1 4]))]
+      (is (= result [[0 4 4 7 9 9] 1 [0 1 1 1 1 1]])))
+    ))
 
 (deftest reduce-test
   (testing "reduce-test"
+
+    ; without tracking
     (let [result (reduce deduce [[0 0 0 0 0 0 0] [4 3]])]
       (is (= result [0 0 0 0 3 3 3 ])))
     (let [result (reduce deduce [[0 0 0 0 0 0 0] [4 3] [3 2]])]
@@ -72,7 +86,16 @@
     (let [result (reduce deduce [[0 0 0 0 0 0 0] [4 3] [3 2] [2 4]])]
       (is (= result [0 0 4 4 4 6 7 ])))
     (let [result (reduce deduce [[0 0 0 0 0 0 0] [4 3] [3 2] [2 4] [3 4]])]
-      (is (= result [0 0 4 4 4 8 8 ])))))
+      (is (= result [0 0 4 4 4 8 8 ])))
+
+    ; with tracking
+    (let [result (reduce deduce-and-track [[[0 0 0 0 0 0] 0 [0 0 0 0 0 0]] [3 5]])]
+      (is (= result [[0 0 0 5 5 5] 1 [0 0 0 1 1 1]])))
+    (let [result (reduce deduce-and-track [[[0 0 0 0 0 0] 0 [0 0 0 0 0 0]] [3 5] [2 3]])]
+      (is (= result [[0 0 3 5 5 8] 2 [0 0 2 1 1 3]])))
+    (let [result (reduce deduce-and-track [[[0 0 0 0 0 0] 0 [0 0 0 0 0 0]] [3 5] [2 3] [1 4]])]
+      (is (= result [[0 4 4 7 9 9] 3 [0 4 6 5 5 7]])))
+    ))
 
 (deftest build-args-test
   (testing "build-args-test"
@@ -85,11 +108,82 @@
       (is (= (get result 3) [2 4]))
       (is (= (get result 4) [3 4])))))
       
+(deftest back-track-test
+  (testing "back-track-test"
+    (let [result (vec (back-track [[0 4 6 5 5 7] [] [3 2 1]] 2))]
+      (is (vector? result))
+      (is (= (second result) [2])))
+
+    (let [result (vec (run-back-track [0 4 6 5 5 7] [3 2 1]))]
+       (is (vector? result))
+       (is (= result [2 0])))))
+
 (deftest get-optimal-value-test
-  (testing "get-optimal-value"
+  (testing "get-optimal-value-test"
+    ; base case (no capacity)
+    (let [result (get-optimal-value [4 3 2 3] [3 2 4 4] 0)]
+      (is (= result 0)))
+    
+    ; base case (no items)
+    (let [result (get-optimal-value [] [] 6)]
+      (is (= result 0)))
+
+    (let [result (get-optimal-value [3 2 1] [5 3 4] 5)]
+      (is (= result 9)))
+
     (let [result (get-optimal-value [4 3 2 3] [3 2 4 4] 6)]
       (is (= result 8)))
+    
     (let [result (get-optimal-value 
-      [9 13 153  50 15 68 27 39 23 52 11 32 24 48 73 42 43 22  7 18  4 30] 
+      [9   13 153  50 15 68 27 39 23 52 11 32 24 48 73 42 43 22  7 18  4 30] 
       [150 35 200 160 60 45 60 40 30 10 70 30 15 10 40 70 75 80 20 12 50 10] 400)]
       (is (= result 1030)))))
+
+(deftest get-optimal-solution-test
+  (testing "get-optimal-solution-test"
+
+    ; base case (no capacity)
+    (let [result (vec (get-optimal-solution [4 3 2 3] [3 2 4 4] 0))]
+      (is (= result [0 nil nil])))
+    
+    ; base case (no items)
+    (let [result (vec (get-optimal-solution [] [] 6))]
+      (is (= result [0 nil nil])))
+
+    (let [result (vec (get-optimal-solution [3 2 1] [5 3 4] 5))]
+      (is (= (first result) 9))
+      (is (= (vec (last result)) [2 0])))
+
+    (let [result (vec (get-optimal-solution [4 3 2 3] [3 2 4 4] 6))]
+      (is (= (first result) 8))
+      (is (= (vec (last result)) [3 2])))
+    
+    ; Comment/uncomment when needs as this might generate alot of text on the shell when things break
+
+    (let [result (get-optimal-solution 
+      [  9 13 153  50 15 68 27 39 23 52 11 32 24 48 73 42 43 22  7 18  4 30] 
+      [150 35 200 160 60 45 60 40 30 10 70 30 15 10 40 70 75 80 20 12 50 10] 400)]
+      (is (= (first result) 1030))
+      (is (= (vec (last result)) [20 18 17 16 15 10 6 4 3 2 1 0])))
+
+      ; solution
+      ;0 luke       9   150
+      ;1 anthony    13  35
+      ;2 candice    153 200
+      ;3 dorothy    50  160
+      ;4 puppy      15  60
+      ;6 randal     27  60
+      ;10 marc      11  70
+      ;15 grumpkin  42  70
+      ;16 dusty     43  75
+      ;17 grumpy    22  80
+      ;18 eddie     7   20
+      ;20 sally     4   50
+
+
+(reduce deduce-and-track 
+  [[(gen-item 0 0 400) 0 (gen-item 0 0 400) ]
+    [9 150][13 35][153 200][50 160][15 60][68 45][27 60][39 40][23 30][52 10][11 70][32 30][24 15][48 10][73 40][42 70][43 75][22 80][7 20][18 12][4 50][30 10]])
+       
+
+))
