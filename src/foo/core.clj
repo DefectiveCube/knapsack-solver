2a1657409a182c8b8e70d9a259814f15adef8784
Kirk Dietz <kdietz1@email.davenport.edu>
2015-07-26 22:55:27 -0400

    Working version. Added tests. Finds optimal value but does not do back-tracing

--- src/foo/core.clj
+++ src/foo/core.clj
@@ -1,74 +1,200 @@
 (ns foo.core)
 
-(defn fileExists [p] 
-  (. System.IO.File Exists p))
+(defn join-values "joins two values as a vector" [a b]
+  [a b])
 
-(defn parse-int [n] 
-  (if-not (string? n) (throw (ArgumentException. "Argument must be a string")))
-  (. Int64 Parse n))
+(defn inc-non-zero "increases a by b, only if a is not zero" [a b]
+  (if-not (= a 0)
+    (+ a b)
+    (* 0 0)))
 
-(defn to-int-array [s]
-  (if-not (string? s) (throw (ArgumentException. "Argument must be a string")))
-  (int-array (.Split s (char-array " ") StringSplitOptions/RemoveEmptyEntries)))
+(defn inc-non-zeroes "increases non-zero values of collection using scalar value. returns vector" 
+  [coll scalar]
+  (map inc-non-zero coll (repeat (count coll) scalar)))
+
+(defn gen-item "generates sequence for given weight value and capacity" [weight value capacity]
+  (lazy-cat (take weight (repeat 0)) (repeat (- (inc capacity) weight) value)))
+
+(defn gen-items [weights values capacity]
+  (let [n (count weights)]
+    (for [i (range 0 n)]
+      (gen-item (get weights i) (get values i) capacity))))
+
+(defn shift "shifts values in a collection" [previous shift]
+  (let [capacity (count previous)]
+    (take capacity (lazy-cat (repeat shift 0) previous))))
+
+(defn get-next [coll weight value]
+  (map max
+    coll
+    (let [c (dec (count coll))]
+      (gen-item weight value c))    
+    (inc-non-zeroes (shift coll weight) value)))
+
+(defn deduce [coll args]
+  (if-not (vector? args) (throw (ArgumentException. "args should be a vector")))
+  (vec (get-next coll (get args 0) (get args 1))))
+
+(defn build-args [weights values capacity]  
+  (vec (conj 
+    (map join-values weights values)
+    (gen-item 0 0 capacity))))
+
+(defn get-optimal-value [weights values capacity]
+  (let [args (build-args weights values capacity)]
+    (last (reduce deduce args))))
+
+;(defn fileExists [p] 
+;  (. System.IO.File Exists p))
+
+;(defn parse-int [n] 
+;  (if-not (string? n) (throw (ArgumentException. "Argument must be a string")))
+;  (. Int64 Parse n))
+
+;(defn to-int-array [s]
+;  (if-not (string? s) (throw (ArgumentException. "Argument must be a string")))
+;  (long-array (.Split s (char-array " ") StringSplitOptions/RemoveEmptyEntries)))
   
-(defn pathCombine "Create an absolute path (limited to CurrentDirectory)" [p] 
-  (. System.IO.Path Combine (. Environment CurrentDirectory) "src" "foo" p))
+;(defn pathCombine "Create an absolute path (limited to CurrentDirectory)" [p] 
+;  (. System.IO.Path Combine (. Environment CurrentDirectory) "src" "foo" p))
 
-(defn indexOfMin "returns the index of the lowest value" [v]
-	(first (apply min-key second (map-indexed vector v))))
+;(defn indexOfMin "returns the index of the lowest value" [v]
+;	(first (apply min-key second (map-indexed vector v))))
 
-(defn valueOfPreviousColumn "Returns the value of the element in the previous column" [arr x y] 
-    (if-not arr (throw (NullReferenceException. "Array cannot be null")))
-    (if-not (integer? x) (throw (ArgumentException. "x must be an integer")))
-    (if-not (integer? y) (throw (ArgumentException. "y must be an integer")))
+;(defn valueOfPreviousColumn "Returns the value of the element in the previous column" [arr x y] 
+;    (if-not arr (throw (NullReferenceException. "Array cannot be null")))
+;    (if-not (integer? x) (throw (ArgumentException. "x must be an integer")))
+;    (if-not (integer? y) (throw (ArgumentException. "y must be an integer")))
 
-    (if(> x 0) (aget - x 1) 0))
+;    (if(> x 0) (aget - x 1) 0))
 
 
-(defn optimize [w v c n] 
-  (for [i (range 1 n)]
-    (for [j (range c)]
-      (println "(" i "," j ")")
-)))
 
-(defn knapsack "0/1 knapsack optimization" [w v c n]
+;(defn back-track [arr]
+
+;  );
+
+;(defn optimal-array-recursive [w v c n]
+
+;  )
+
+;(defn optimal-array [w v c n]
+;  (println "n: " n "c: " c)
+;  (let [rows (int-array (+ 2 c)) cols (int-array (+ 2 n))]
+;    (let [arr (to-array-2d [cols rows])]
+;      (println (alength arr))
+
+;      (doseq [i (range 0 (+ 1 n))]
+;        (doseq [j (range 0 (+ 1 c))]
+;          (let [h (- i 1)]
+;            (println "i: " i "j: " j)
+;            (cond
+;              (or (= i 0) (= j 0))      ; first row/column are always zero
+                ; these are jagged arrays
+;                (aset arr i j 0)        ; arr[i][j] = 0
+;              (<= (aget w h) j)         ; true: weight can fit
+                ; arr[i][j] = max(v[h] + arr[h][j-w[h]],  arr[h][j]);
+ ;               (aset arr i j
+  ;                max(
+   ;                 (+
+    ;                  (aget v h)                      ; v[h]
+     ;                 (aget arr h (- j (aget w h))))  ; arr[h][j-w[h]]
+      ;              (aget arr h j)
+       ;           ))
+        ;    :else
+         ;     (aset arr i j (aget arr h j)) ; arr[i][j] = arr[i-1][j]
+          ;  )
+          ;)
+        ;)
+;      )
+;  arr)))
+
+;(defn optimize [w v c n] 
+;  (let [arr (long-array c 0)]
+;    (doseq [i (range 0 n)]
+;      (println (aget w i) "|" (aget v i))
+
+      ; i > 0
+      ; w[i] + w[i-1] <= c
+
+;      (let [weight (aget w i) canAdd (and (> i 0) (+ weight (aget w (- i 1))))]
+;        (doseq [j (range 0 c)]
+;          (if (<= weight j)
+;            (let 
+;              [
+;                prev (aget arr j) 
+;                maxValue (+ (aget arr (- j weight)) (aget v i))
+;              ]
+;              (if (> maxValue prev)
+;                (aset arr j maxValue)
+;                (println "Row:" j " " prev " --> " maxValue)
+              ;  (
+              ;    (aset arr row maxValue)
+              ;    (aset keep row col true)
+;            ))))))
+              
+
+    ;(spit "log.txt" (. String Join "," arr))
+;    (println (apply max arr))
+;    [arr nil]
+;    ))
+    
+        ; only consider changes if item can fit
+        ;(if (<= weight row)
+         ; (let [maxValue (+ (aget arr (- row weight)) (aget v col))]
+          ;  (if (> maxValue prev)
+           ;   (
+            ;    (aset arr row maxValue)
+             ;   (aset keep row col true)
+              ;  (println "row:" row " col:" col " " maxValue)
+              ;)
+            ;)))))))
+
+;(defn knapsack "0/1 knapsack optimization" [w v c n]
 
   ; arguments should be of a specific type
-  (if-not (or (vector? w) (array? w)) (throw (ArgumentException. "Weights 'w' should be a vector or an array")))
-  (if-not (or (vector? v) (array? v)) (throw (ArgumentException. "Values 'v' should be a vector or an array")))
-  (if-not (integer? n) (throw (ArgumentException. "Number of Units 'n' should be an integer")))
-  (if-not (integer? c) (throw (ArgumentException. "Capacity 'c' should be an integer")))
+;  (if-not (array? w) (throw (ArgumentException. "Weights 'w' should be an array")))
+;  (if-not (array? v) (throw (ArgumentException. "Values 'v' should be an array")))
+;  (if-not (integer? n) (throw (ArgumentException. "Number of Units 'n' should be an integer")))
+;  (if-not (integer? c) (throw (ArgumentException. "Capacity 'c' should be an integer")))
 
   ; obey the laws of physics
-  (if (< c 0) (throw (ArgumentOutOfRangeException. "Capacity must be non-negative")))
-  (if (< n 0) (throw (ArgumentOutOfRangeException. "Count must be non-negative")))
+;  (if (< c 0) (throw (ArgumentOutOfRangeException. "Capacity must be non-negative")))
+;  (if (< n 0) (throw (ArgumentOutOfRangeException. "Count must be non-negative")))
 
   ; verification
-  (if-not (= (count w) n) (throw (ArgumentException. "Weights is wrong size")))
-  (if-not (= (count v) n) (throw (ArgumentException. "Values is wrong size")))
-  (if-not (= (count w) (count v)) (throw (Exception. "Weights and Values must have the same amount of elements")))  
+;  (if-not (= (count w) n) (throw (ArgumentException. "Weights is wrong size")))
+;  (if-not (= (count v) n) (throw (ArgumentException. "Values is wrong size")))
+;  (if-not (= (count w) (count v)) (throw (Exception. "Weights and Values must have the same amount of elements")))  
 
-  (if (and (> c 0) (> n 0)) (optimize w v c n) 0))
+;   (if (and (> c 0) (> n 0)) 
 
-(defn knapsackFile "0/1 knapsack optimization using input file" [filepath]  
+;     (optimal-array w v c n) 
+
+
+;     0))
+
+; (defn knapsackFile "0/1 knapsack optimization using input file" [filepath]  
   
-  ; path must be a string
-  (if-not (string? filepath) (throw (ArgumentException. "path must be a string")))
+;   ; path must be a string
+;   (if-not (string? filepath) (throw (ArgumentException. "path must be a string")))
 
-  (let [p (pathCombine filepath)]
+;   (let [p (pathCombine filepath)]
 
-    ; path must be to an existing file
-    (if-not (fileExists p) (throw (System.IO.FileNotFoundException. p)))  
+;     ; path must be to an existing file
+;     (if-not (fileExists p) (throw (System.IO.FileNotFoundException. p)))  
 
-    ; Read all lines
-    (let [lines (. System.IO.File ReadAllLines p)] 
+;     ; Read all lines
+;     (let [lines (. System.IO.File ReadAllLines p)] 
 
-      ; files must contain 3 lines
-      (if-not (= (count lines) 3) (throw (Exception. "Malformed Lines")))
+;       ; files must contain 3 lines
+;       (if-not (= (count lines) 3) (throw (Exception. "Malformed Lines")))
 
-      (let [capacity (parse-int (aget lines 0)) weights (to-int-array (aget lines 1)) values (to-int-array (aget lines 2))]        
-        (knapsack weights values capacity (count weights))))))
+;       (let [capacity (parse-int (aget lines 0)) weights (to-int-array (aget lines 1)) values (to-int-array (aget lines 2))]        
+;         (knapsack weights values capacity (count weights))))))
 
-(defn -main
-  [& args]
-  (apply println "Received args:" args))
+; (defn -main
+;   [& args]
+;   (if (string? args)
+;     (apply println "Received args:" args))
+;     (println "Argument must be a string to a single file"))
--- test/foo/core_test.clj
+++ test/foo/core_test.clj
@@ -2,117 +2,94 @@
   (:use clojure.test
     foo.core))
 
-;expected to PASS
-(deftest TruthTest
-  (testing "base"
-    (is true)))
+(deftest increase-non-zero
+  (testing "increase-non-zero test"
+    (is (function? inc-non-zero))
+    (is (= 3 (inc-non-zero 1 2)))
+    (is (= 0 (inc-non-zero 0 3)))))
 
-; expected to PASS
-(deftest base-test-1
-  (testing "Base Test #1"
-    (knapsack 
-      [  9 13 153  50 15 68 27 39 23 52 11 32 24 48 73 42 43 22  7 18  4 30]
-      [150 35 200 160 60 45 60 40 30 10 70 30 15 10 40 70 75 80 20 12 50 10]
-      400
-      22)
-    (println "Base Test #1: Passed (Given Data)")))
+(deftest increase-non-zeroes
+  (testing "increase-non-zeroes test"
+    (is (function? inc-non-zeroes))
+    (let [result (vec (inc-non-zeroes [0 1 2 0] 4))]
+      (is (= 0 (get result 0)))
+      (is (= 5 (get result 1)))
+      (is (= 6 (get result 2)))
+      (is (= 0 (get result 3))))))
 
-; expected to PASS
-(deftest base-test-2
-  (testing "Base Test #2"
-    (let [value (knapsack [] [] 0 0)] 
-      (is (= value 0)))
-    (println "Base Test #2: Passed (Special Case)")))
+(deftest generate-item-test
+  (testing "gen-item-test"
+    (let [result (vec (gen-item 4 3 6))]
+      (is (= (count result) 7))
+      (is (= [0 0 0 0 3 3 3])))))
 
-; expected to PASS (same as base-test-1)
-(deftest file-test-1
-  (testing "File Test #1"
-    (is (function? knapsackFile))
-    (try
-      (knapsackFile "test.txt")
-      (catch Exception e
-        (is false (.Message e))))
-    (println "File Test #1: Passed (Given Data)")))         
+(deftest generate-items-test
+  (testing "gen-items-test"
+    (let [result (gen-items [4 3 2 3] [3 2 4 4] 6)]
+      (is (= (count result) 4)))))      
 
-; expected to FAIL because file does not exist
-(deftest file-test-2
-  (testing "File Test #2: Missing File"
-    (is (function? knapsackFile))    
-    (try
-      (knapsackFile "test2.txt")
-      (assert false "Exception not thrown")
-      (catch System.IO.FileNotFoundException e)
-      (catch Exception e 
-        (is false (.Message e))))
-    (println "File Test #2: Passed (Missing File)")))
+(deftest shift-test
+  (testing "shift-test"
+    (is (function? shift))
+    (let [result (vec (shift [0 0 0 0 0 0 0] 4))]
+      (is (= result [0 0 0 0 0 0 0])))
+    (let [result (vec (shift [0 0 0 0 3 3 3] 3))]
+      (is (= result [0 0 0 0 0 0 0])))
+    (let [result (vec (shift [0 0 0 2 3 3 3] 2))]
+      (is (= result [0 0 0 0 0 2 3])))
+    (let [result (vec (shift [0 0 4 4 4 6 7] 3))]
+      (is (= result [0 0 0 0 0 4 4])))))
 
-(deftest file-test-3
-  (testing "File Test #3: Small Dataset"
-    (is (function? knapsackFile))   
-    (try
-      (knapsackFile "test3.txt")
-      (catch Exception e
-        (is false (.Message e))))
-    (println "File Test #3: Passed (Small Dataset)")))
+(deftest get-next-test
+  (testing "get-next-test"
+    (is (function? get-next))
+    (let [result (vec (get-next [0 0 0 0 0 0 0] 4 3))]
+      (is (= result [0 0 0 0 3 3 3])))
+    (let [result (vec (get-next [0 0 0 0 3 3 3] 3 2))]
+      (is (= result [0 0 0 2 3 3 3])))
+    (let [result (vec (get-next [0 0 0 2 3 3 3] 2 4))]
+      (is (= result [0 0 4 4 4 6 7])))
+    (let [result (vec (get-next [0 0 4 4 4 6 7] 3 4))]
+      (is (= result [0 0 4 4 4 8 8])))))   
 
-(deftest file-test-4
-  (testing "File Test #4: Mismatched Arrays"
-    (is (function? knapsackFile))
-    (try
-      (knapsackFile "test4.txt")
-      (is false "Exception not thrown")
-      (catch Exception e))
-    (println "File Test #4: Passed (Mismatched Arrays)")))
+(deftest deduce-test
+  (testing "deduce-test"
+    (let [result (vec (deduce [0 0 0 0 0 0 0] [4 3]))]
+      (is (= result [0 0 0 0 3 3 3])))
+    (let [result (vec (deduce [0 0 0 0 3 3 3] [3 2]))]
+      (is (= result [0 0 0 2 3 3 3])))
+    (let [result (vec (deduce [0 0 0 2 3 3 3] [2 4]))]
+      (is (= result [0 0 4 4 4 6 7])))
+    (let [result (vec (deduce [0 0 4 4 4 6 7] [3 4]))]
+      (is (= result [0 0 4 4 4 8 8])))))
 
-(deftest file-test-5
-  (testing "File Test #5: Missing Lines"
-    (try
-      (knapsackFile "test5.txt")
-      (is false "Exception not thrown")
-      (catch Exception e))
-    (println "File Test #5: Passed (Missing Lines)")))
+(deftest reduce-test
+  (testing "reduce-test"
+    (let [result (reduce deduce [[0 0 0 0 0 0 0] [4 3]])]
+      (is (= result [0 0 0 0 3 3 3 ])))
+    (let [result (reduce deduce [[0 0 0 0 0 0 0] [4 3] [3 2]])]
+      (is (= result [0 0 0 2 3 3 3 ])))
+    (let [result (reduce deduce [[0 0 0 0 0 0 0] [4 3] [3 2] [2 4]])]
+      (is (= result [0 0 4 4 4 6 7 ])))
+    (let [result (reduce deduce [[0 0 0 0 0 0 0] [4 3] [3 2] [2 4] [3 4]])]
+      (is (= result [0 0 4 4 4 8 8 ])))))
 
-(deftest file-test-6
-  (testing "File Test #6: Malformed Lines"
-    (is (function? knapsackFile))
-    (try
-      (knapsackFile "test6.txt")
-      (is false "Exception not thrown")
-      (catch Exception e
-        (is (= (.Message e) "Malformed Lines"))))
-    (println "File Test #6: Passed (Malformed Lines)")))
-
-; passing an object that isn't a string as a path
-(deftest file-test-7
-  (testing "File Test #7: Invalid Characters in Path")
-  )
-
-(deftest file-test-8
-  (testing "File Test #8: Wrong Type"))
-
-; invalid characters in path
-
-; Ancillary functions
-
-(deftest split-string-by-whitespace
-  (testing "String Split Test"
-    (let [s (to-int-array "0 1 2 3 4 5")]
-      (is (= (count s) 6)))))
-
-(deftest parse-integer
-  (testing "Parsing Integer"
-    (let [value (parse-int "40")]
-      (is (function? parse-int))
-      (is (= 40 value)))))
-
-(deftest index-of-minvalue
-  (testing "MinValue"
-    (let [minValue (indexOfMin [5 6 3 1 5])]
-      (is (= minValue 3)))
-    (let [minValue (indexOfMin [0 1 2 3 4])]
-      (is (= minValue 0)))))
-
-;(deftest file-exists
-;  (testing "File Exists?"
-;    (let [result (. System.IO.File Exists "C:\\audio.log")] ; FIXME: this only works on Windows
-;      (is (= result true)))))
+(deftest build-args-test
+  (testing "build-args-test"
+    (let [result (build-args [4 3 2 3] [3 2 4 4] 6)]
+      (is (= (vector? result)))
+      (is (= (count result) 5))
+      (is (= (get result 0) [0 0 0 0 0 0 0]))
+      (is (= (get result 1) [4 3]))
+      (is (= (get result 2) [3 2]))
+      (is (= (get result 3) [2 4]))
+      (is (= (get result 4) [3 4])))))
+      
+(deftest get-optimal-value-test
+  (testing "get-optimal-value"
+    (let [result (get-optimal-value [4 3 2 3] [3 2 4 4] 6)]
+      (is (= result 8)))
+    (let [result (get-optimal-value 
+      [9 13 153  50 15 68 27 39 23 52 11 32 24 48 73 42 43 22  7 18  4 30] 
+      [150 35 200 160 60 45 60 40 30 10 70 30 15 10 40 70 75 80 20 12 50 10] 400)]
+      (is (= result 1030)))))
